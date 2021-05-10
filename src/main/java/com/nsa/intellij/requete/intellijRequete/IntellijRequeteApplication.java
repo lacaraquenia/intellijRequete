b3f556a0233957eae101931ede1c4c29bcbc247f
Cara Quenia <lacaraquenia@yahoo.com>
2021-05-10 09:59:18 +0200

    initial commit

---
+++ src/main/java/com/nsa/intellij/requete/intellijRequete/IntellijRequeteApplication.java
@@ -0,0 +1,13 @@
+package com.nsa.intellij.requete.intellijRequete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellijRequeteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellijRequeteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nsa/intellij/requete/intellijRequete/IntellijRequeteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsa.intellij.requete.intellijRequete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellijRequeteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
